8a945b6951d2918c8d5d24bb79b6d621a5ba6629
Prathamesh More <pprathameshmore@outlook.com>
2020-08-24 17:19:49 +0530

    Update index.js

--- src/Configs/index.js
+++ src/Configs/index.js
@@ -1,3 +1,3 @@
 //exports.API_URL = "http://localhost:3000";
-exports.API_URL = "http://fshare-files.azurewebsites.net";
-exports.AUTH_SERVER = "http://fshare-auth.azurewebsites.net";
+exports.API_URL = "https://fshare-files.azurewebsites.net";
+exports.AUTH_SERVER = "https://fshare-auth.azurewebsites.net";
